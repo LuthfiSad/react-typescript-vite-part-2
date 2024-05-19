e5f9dc0119ae2b408dc8210ac8185715a6a8b97f
muhamad luthfi sadli <luthfisad1806@gmail.com>
2024-05-19 17:42:22 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# React + TypeScript + Vite Part 2
+
+#### Learn React Typescript Part 2 from channel Youtube Techinfo YT
+Link : [React Typescript Tutorials In Hindi](https://youtube.com/playlist?list=PLuHGmgpyHfRx9e7yeOaaL79_L_mBFBHLc&si=SPJKJchAlb_wx8uP)
+
+### Available Scripts
+
+In the project directory, you can run:
+
+#### `npm run dev`
+
+Runs the app in development mode.\
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+#### `npm run build`
+
+Builds the app for production to the `dist` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+#### `npm run lint`
+
+Runs ESLint to check for linting errors in the project. It will report unused directives and stop if there are any warnings.
+
+#### `npm run preview`
+
+Serves the production build locally for preview.\
---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+import About from "./components/About";
+// import UserList from "./components/UserList";
+import Users from "./components/Users";
+// import AuthUser from "./components/AuthUser";
+import Login from "./components/Login";
+import Navabr from "./components/Navbar";
+import UserDetail from "./components/UserDetail";
+// import Counter from "./components/Counter";
+// import Youtube from "./components/Youtube";
+// import ClassComp from "./components/ClassComp";
+// import FunComp from "./components/FunComp";
+
+function App() {
+  return (
+    <>
+      <Navabr />
+      <Routes>
+        <Route path="/" element={<Users />} />
+        <Route path="/users/:id" element={<UserDetail />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
